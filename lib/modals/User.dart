3cd2033c2c19fd0f4067c3dc02fe8d508054ab03
Pierre <pierre.tr@hotmail.fr>
2021-01-15 17:23:56 +0100

    chore: remove absences from User modal

--- lib/modals/User.dart
+++ lib/modals/User.dart
@@ -6,14 +6,12 @@ class User {
     final String id;
     final String name;
     final String color;
-    final List<dynamic> absences;
     final DateTime createDate;
 
     const User({
         @required this.id,
         @required this.name,
         this.color,
-        this.absences,
         this.createDate
     });
 
@@ -22,7 +20,6 @@ class User {
         id: json['_id'] != null ? json['_id'] : 'unknow',
         name: json['name'] != null ? json['name'] : 'unknow',
         color: json['color'],
-        absences: json['absences'],
         createDate: json['createDate'] != null ? DateTime.parse(json['createDate']) : null
     );
 
@@ -31,7 +28,6 @@ class User {
         '_id': id,
         'name': name,
         'color': color,
-        'absences': absences,
         'createDate': createDate != null ? createDate.toString() : null
     };
 }
